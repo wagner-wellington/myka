0b9370b7ca1f39a79ed645ed521ce2426cc9a31f
wagnerwellington <wag1well@hotmail.com>
2016-05-06 21:17:06 -0300

    Indicação correta do caminho dos arquivos js

--- application/views/pg_js.php
+++ application/views/pg_js.php
@@ -1,2 +1,41 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!--   Core JS Files   -->
+    <script src="<?php echo base_url();  ?>assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="<?php echo base_url();  ?>assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+	<!--  Checkbox, Radio & Switch Plugins -->
+	<script src="<?php echo base_url();  ?>assets/js/bootstrap-checkbox-radio-switch.js"></script>
+
+	<!--  Charts Plugin -->
+	<script src="<?php echo base_url();  ?>assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="<?php echo base_url();  ?>assets/js/bootstrap-notify.js"></script>
+
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+	<script src="<?php echo base_url();  ?>assets/js/light-bootstrap-dashboard.js"></script>
+
+    <!--  Sharrre plugin	 -->
+    <script src="<?php echo base_url();  ?>assets/js/jquery.sharrre.js"></script>
+
+	<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+	<script src="<?php echo base_url();  ?>assets/js/demo.js"></script>
+
+	<script type="text/javascript">
+    	$(document).ready(function(){
+
+        	demo.initChartist();
+
+        	$.notify({
+            	icon: 'pe-7s-gift',
+            	message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+
+            },{
+                type: 'info',
+                timer: 4000
+            });
+
+    	});
+	</script>
